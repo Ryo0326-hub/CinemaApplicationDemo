61418f33930d00d740870dadc8eea569be9f1d41
Ryo Kitano <rkitano0326@gmail.com>
2023-09-04 17:17:45 +0400

    Initial Commit - Tickets

--- src/main/java/com/ryo/domain/Ticket.java
+++ src/main/java/com/ryo/domain/Ticket.java
@@ -1,2 +1,28 @@
-package com.ryo.domain;public class Ticket {
+package com.ryo.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ticket {
+
+    @Id
+    @GeneratedValue
+    private Long ID;
+
+    @Column(name = "Ticket_Price")
+    private Double price;
+    @Column(length = 20,name = "Movie_Name")
+    private String movieName;
+
+    @Column(name = "Seat_Number")
+    private Double seatNumber;
+
+
 }
