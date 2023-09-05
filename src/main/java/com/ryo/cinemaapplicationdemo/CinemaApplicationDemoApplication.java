13788120eb7cd529d94ada9ccdcc8ed31678e2c8
hasanli-orkhan <orkhan.hasanli.92@gmail.com>
2023-09-05 18:42:34 +0400

    chore: code review

--- src/main/java/com/ryo/cinemaapplicationdemo/CinemaApplicationDemoApplication.java
+++ src/main/java/com/ryo/cinemaapplicationdemo/CinemaApplicationDemoApplication.java
@@ -8,27 +8,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CinemaApplicationDemoApplication {
 
-    //UserRepository
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    TicketRepository ticketRepository;
-
-    @Autowired
-    ScreeningRepository screeningRepository;
-
-    @Autowired
-    MovieListingsRepository movieListingsRepository;
-
-    @Autowired
-    DirectorRepository directorRepository;
-
-    @Autowired
-    CinemaRepository cinemaRepository;
-
-    @Autowired
-    BookingRepository bookingRepository;
+//    //UserRepository
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Autowired
+//    TicketRepository ticketRepository;
+//
+//    @Autowired
+//    ScreeningRepository screeningRepository;
+//
+//    @Autowired
+//    MovieListingsRepository movieListingsRepository;
+//
+//    @Autowired
+//    DirectorRepository directorRepository;
+//
+//    @Autowired
+//    CinemaRepository cinemaRepository;
+//
+//    @Autowired
+//    BookingRepository bookingRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplicationDemoApplication.class, args);
